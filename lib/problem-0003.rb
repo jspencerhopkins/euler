1d52362638ce0ccbcc04717b5111a3cf046644e5
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-07 13:46:09 -0400

    Problem 3 solved...again all thanks to asheren

--- lib/problem-0003.rb
+++ lib/problem-0003.rb
@@ -1,3 +1,14 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
+
+require 'prime'
+
+module Problem3
+
+  def self.prime(num)
+    primes = Prime.prime_division(num)
+    primes.last[0]
+  end
+
+end
--- spec/problem-0003_spec.rb
+++ spec/problem-0003_spec.rb
@@ -1,3 +1,17 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
+
+require './lib/problem-0003'
+
+describe 'answer' do
+  
+  it 'will have the largest prime factor for 13195' do
+    expect(Problem3.prime(13195)).to eq 29
+  end
+
+  it 'will have the largest prime factor for 600851475143' do
+    expect(Problem3.prime(600851475143)).to eq 6857
+  end
+
+end
