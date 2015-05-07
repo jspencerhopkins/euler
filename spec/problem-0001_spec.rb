27f00c8ea20fe861aedfee4299548517bbd70ec1
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-06 20:12:27 -0400

    Add test for problem 1 - borrowed from asheren

--- spec/problem-0001_spec.rb
+++ spec/problem-0001_spec.rb
@@ -1,1 +1,18 @@
+# If we list all the natural numbers below 10 
+# that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
+require './lib/problem-0001'
+
+describe 'solution' do
+
+  it 'sums the multiples of 3 and 5 under 10' do
+    expect(Problem1.multiples(10)).to eq 23
+  end
+
+  it 'sums the multiples of 3 and 5 under 1000' do
+    expect(Problem1.multiples(1000)).to eq 233168
+  end
+
+end
