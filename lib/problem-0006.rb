769d05cd24c8945619e2fb7f000ac517b2431f5d
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-08 14:21:40 -0400

    holy crap - I solved it on my own!

--- lib/problem-0006.rb
+++ lib/problem-0006.rb
@@ -1,11 +1,30 @@
 # The sum of the squares of the first ten natural numbers is,
 
-# 12 + 22 + ... + 102 = 385
+# 1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
 
-# (1 + 2 + ... + 10)2 = 552 = 3025
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
 # Hence the difference between the sum of the squares of the first 
 # ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 # Find the difference between the sum of the squares of the 
 # first one hundred natural numbers and the square of the sum.
+
+module Problem6
+
+  def self.answer(range)
+
+    sum_of_sq = 0
+    range.each do |n|
+      s = n**2
+      sum_of_sq += s
+    end
+    
+    sum_range = range.reduce(:+)
+    sq_of_sum = sum_range ** 2
+
+    return sq_of_sum - sum_of_sq
+
+  end
+
+end
--- spec/problem-0006_spec.rb
+++ spec/problem-0006_spec.rb
@@ -1,11 +1,26 @@
 # The sum of the squares of the first ten natural numbers is,
 
-# 12 + 22 + ... + 102 = 385
+# 1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
 
-# (1 + 2 + ... + 10)2 = 552 = 3025
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
 # Hence the difference between the sum of the squares of the first 
 # ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 # Find the difference between the sum of the squares of the 
 # first one hundred natural numbers and the square of the sum.
+
+require './lib/problem-0006'
+
+describe 'difference between' do
+
+  it 'sum of squares and the square of sum of the 1st 10 natural #s' do
+    expect(Problem6.answer(1..10)).to eq 2640
+  end
+
+  it 'sum of squares and the square of sum of the 1st 100 natural #s' do
+    expect(Problem6.answer(1..100)).to eq  25164150
+  end
+
+end 
+
