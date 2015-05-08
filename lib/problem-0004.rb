bc3cfdd42a74e49cebf291dda7b1d93188ce0624
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-07 21:38:26 -0400

    Problem 4 solved - with help

--- lib/problem-0004.rb
+++ lib/problem-0004.rb
@@ -4,3 +4,18 @@
 
 # Find the largest palindrome made from the 
 # product of two 3-digit numbers.
+
+module Problem4
+
+  def self.answer(pal_range)
+    max = 0
+    (pal_range).each do |a|
+      (a..pal_range.end).each do |b|
+        product = a*b
+        max = [max, product].max if product.to_s == product.to_s.reverse
+      end
+    end
+    return max
+  end
+
+end
--- spec/problem-0004_spec.rb
+++ spec/problem-0004_spec.rb
@@ -4,3 +4,17 @@
 
 # Find the largest palindrome made from the 
 # product of two 3-digit numbers.
+
+require './lib/problem-0004'
+
+describe "largest palindrome" do
+
+  it 'finds the largest palindrome of 2 digit numbers' do
+    expect(Problem4.answer(10...99)).to eq 9009
+  end
+
+  it 'finds the largest palindrome of 3 digit numbers' do
+    expect(Problem4.answer(100...1000)).to eq 906609
+  end
+
+end
