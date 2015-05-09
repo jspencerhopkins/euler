491e0b7816a4f62d9b960ba72c26b432dd1a49fe
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-08 22:21:24 -0400

    Problem 9 test and framework ready

---
+++ lib/problem-0009.rb
@@ -0,0 +1,17 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+module Problem9
+
+  def self.triplet_product(triplet_sum)
+
+
+
+  end
+
+end
---
+++ spec/problem-0009_spec.rb
@@ -0,0 +1,21 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+require './lib/problem-0009'
+
+describe 'The product of a Pythagorean triplet' do
+
+  it 'of a=3, b=4, c=5 -> has sum = 3+4+5 and product = 3*4*5' do
+    expect(Problem9.triplet_product(12)).to eq 60
+  end
+
+  it 'has sum = 1,000 -> find product' do
+    expect(Problem9.triplet_product(1000)).to eq 31875000
+  end
+
+end
