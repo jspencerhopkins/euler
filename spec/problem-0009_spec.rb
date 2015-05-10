a23e6a6fc6cf648c306c1cb3563acccbd74d492c
jspencerhopkins <jspencerhopkins@gmail.com>
2015-05-10 14:39:37 -0400

    Coming back to this one - skipping ahead to 13

--- spec/problem-0009_spec.rb
+++ spec/problem-0009_spec.rb
@@ -8,13 +8,13 @@
 
 require './lib/problem-0009'
 
-describe 'The product of a Pythagorean triplet' do
+describe 'Pythagorean triplet (a^2 + b^2 = c^2)' do
 
-  it 'of a=3, b=4, c=5 -> has sum = 3+4+5 and product = 3*4*5' do
+  it ': a=3, b=4, c=5 -> has sum = 3+4+5 and product = 3*4*5' do
     expect(Problem9.triplet_product(12)).to eq 60
   end
 
-  it 'has sum = 1,000 -> find product' do
+  it ': has sum = 1,000 -> find product' do
     expect(Problem9.triplet_product(1000)).to eq 31875000
   end
 
