b702ebd22da9f4aae18d046f9b0a775969783a01
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-05-10 20:50:40 -0400

    Un-comment Problem 7 - start running rspec individually

--- lib/problem-0007.rb
+++ lib/problem-0007.rb
@@ -1,35 +1,35 @@
-# # By listing the first six prime numbers: 
-# # 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# By listing the first six prime numbers: 
+# 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-# # What is the 10,001st prime number?
+# What is the 10,001st prime number?
 
-# def is_prime?(n)
+def is_prime?(n)
   
-#   return false if n < 2
-#   return true if n == 2
+  return false if n < 2
+  return true if n == 2
 
-#   i = 2
-#   while i <= n/2 do
-#     if n % i == 0
-#       return false
-#     end
-#     i += 1
-#   end
+  i = 2
+  while i <= n/2 do
+    if n % i == 0
+      return false
+    end
+    i += 1
+  end
 
-#   return true
-# end
+  return true
+end
 
-# def prime_place(position)
+def prime_place(position)
 
-#   counter = 0
-#   i = 0
+  counter = 0
+  i = 0
 
-#   while counter < position     
-#     i += 1
-#     counter += 1 if is_prime?(i)
-#   end
+  while counter < position     
+    i += 1
+    counter += 1 if is_prime?(i)
+  end
 
-#   return i
+  return i
 
-# end
+end
 
--- spec/problem-0007_spec.rb
+++ spec/problem-0007_spec.rb
@@ -1,18 +1,18 @@
-# # By listing the first six prime numbers: 
-# # 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# By listing the first six prime numbers: 
+# 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-# # What is the 10,0001st prime number?
+# What is the 10,0001st prime number?
 
-# require './lib/problem-0007'
+require './lib/problem-0007'
 
-# describe 'Prime number' do
+describe 'Prime number' do
   
-#   it 'in the 6th place' do
-#     expect(prime_place(6)).to eq 13
-#   end
+  it 'in the 6th place' do
+    expect(prime_place(6)).to eq 13
+  end
 
-#   it 'in the 10,001st place' do
-#     expect(prime_place(10001)).to eq 104743
-#   end
+  it 'in the 10,001st place' do
+    expect(prime_place(10001)).to eq 104743
+  end
 
-# end
+end
