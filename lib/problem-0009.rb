ea34e60eea5be30075d398e7ebf32ecfa1aac0fe
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-05-30 13:28:53 -0400

    Problem 9 - figured it out with the help of johnnycoder.com

--- lib/problem-0009.rb
+++ lib/problem-0009.rb
@@ -6,12 +6,25 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-# module Problem9
+module Problem9
 
-#   def self.triplet_product(triplet_sum)
+  def self.triplet_product(triplet_sum)
 
+    product = 0
 
+    1.upto(triplet_sum) do |a|
+      (a + 1).upto(triplet_sum) do |b|
+        c = triplet_sum - a - b
+        if (a**2 + b**2 == c**2)
+          product = a * b * c
+          break
+        end
+      end
+      break if product > 0
+    end
 
-#   end
+    return product
 
-# end
+  end
+
+end
