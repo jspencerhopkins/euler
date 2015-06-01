4562b9383348b8f0d83b80fbf8286c070ce8e30a
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-06-01 11:23:44 -0400

    25 solved - still trying to figure out how it works - thanks johnny coder

---
+++ spec/problem-0025_spec.rb
@@ -0,0 +1,34 @@
+# The Fibonacci sequence is defined by the recurrence relation:
+
+# Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+# Hence the first 12 terms will be:
+
+# F1 = 1
+# F2 = 1
+# F3 = 2
+# F4 = 3
+# F5 = 5
+# F6 = 8
+# F7 = 13
+# F8 = 21
+# F9 = 34
+# F10 = 55
+# F11 = 89
+# F12 = 144
+# The 12th term, F12, is the first term to contain three digits.
+
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+require './lib/problem-0025'
+
+describe 'Fibonacci number to contain' do
+
+  it '3 digits' do
+    expect(Problem25.answer(3)).to eq 12
+  end
+
+  it '1,000 digits' do
+    expect(Problem25.answer(1000)).to eq 4782
+  end
+
+end
